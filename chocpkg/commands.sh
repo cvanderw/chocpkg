896a4edd7a79079895bdeffabfa9a1614dc923b9
Christopher Van Der Westhuizen <cvander169@gmail.com>
2018-01-02 20:19:14 +0900

    Ensure INSTALL_DIR variable is set in setup.
    This change reorders statements in commands.sh such that the INSTALL_DIR
    variable is set and can thus be safely referenced from buildenv.sh.
    
    This attempts to address issue chocolate-doom/chocpkg#15.

--- chocpkg/commands.sh
+++ chocpkg/commands.sh
@@ -1,7 +1,5 @@
 # This file contains implementations of the commands accepted on the CLI.
 
-. "$CHOCPKG_ROOT/chocpkg/functions.sh"
-. "$CHOCPKG_ROOT/buildenv.sh"
 
 # basic_setup is invoked on script startup to set various environment variables
 # and create basic directories used throughout the rest of the script.
@@ -295,3 +293,7 @@ chocpkg::commands::command() {
 
 chocpkg::commands::basic_setup
 
+# Note that buildenv.sh requires the INSTALL_DIR path to be set, which is only
+# set in basic_setup(), thus delaying invocation of these scripts until then.
+. "$CHOCPKG_ROOT/chocpkg/functions.sh"
+. "$CHOCPKG_ROOT/buildenv.sh"
